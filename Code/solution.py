f41ee82adf45a8663355c5ed53a8ca95c67f2a05
firat tamur <ftamur16@ku.edu.tr>
2020-05-25 00:32:37 +0300

    Part e is done.

--- Code/solution.py
+++ Code/solution.py
@@ -69,7 +69,7 @@ def demand_constraits(lp, variables, demands, products, markets):
     return lp
 
 
-def time_constraints(lp, variables, time, products, markets):
+def time_constraints(lp, variables, time, demands, products, markets):
     """
     Adds constraint function for given lp problem with time dict. 
 
@@ -101,6 +101,106 @@ def time_constraints(lp, variables, time, products, markets):
     return lp
 
 
+def change_product(time, demands, profits):
+    """
+    Changes a product's time, demand and profit values. 
+
+    Parameters:
+    pd.DataFrame, pd.DataFrame, pd.DataFrame  
+
+
+    Returns:
+    pd.DataFrame, pd.DataFrame, pd.DataFrame   
+
+    """
+
+    old = input('Enter product name you want to change: ').upper()
+
+    if old not in list(time['Product'].values):
+        print("Product not found!")
+        return
+
+    new = input("Enter a new product name: ").upper()
+    
+    demand = input("Enter demands for new product (Press enter to keep old demands): ")
+    new_demands = demands.copy()
+
+    if demand:
+        demands = list(map(float, demand.split()))
+        new_demands[old] = demand
+   
+    new_demands.rename(columns={old: new}, inplace=True)
+
+    profit = input("Enter profits for new product (Press enter to keep old profits): ")
+    new_profits = profits.copy()
+
+    if profit:
+        profit = list(map(float, profit.split())) 
+        new_profits[old] = profit
+
+    new_profits.rename(columns={old: new}, inplace=True)
+
+    tm = input("Enter time for new product (Press enter to keep old time): ") 
+    new_time = time.copy()
+
+    if tm:
+        tm = float(tm)
+        new_time.loc[new_time['Product'] == old, 'Time(Mins)'] = tm
+    
+    new_time.loc[new_time['Product'] == old, 'Product'] = new
+
+    return new_time, new_demands, new_profits
+
+
+def introduce_product(time, demands, profits):
+    """
+    Add a new product to tables.
+
+    Parameters:
+    pd.DataFrame, pd.DataFrame, pd.DataFrame  
+
+
+    Returns:
+    pd.DataFrame, pd.DataFrame, pd.DataFrame   
+
+    """
+    new = input("Enter a new product name: ").upper()
+    new_demands = demands.copy()
+
+    demand = input("Enter demands for new product: ")
+
+    if not demand:
+        print("Demands must be provided.")
+
+    demand = list(map(float, demand.split()))
+    demands[new] = demand
+   
+
+    profit = input("Enter profits for new product: ")
+    new_profits = profits.copy()
+
+    if not profit:
+        print("Profits must be provided.")
+        return
+    
+    profit = list(map(float, profit.split()))
+    new_profits[new] = profit
+    
+    time = input("Enter time for new product: ") 
+    new_time = time.copy()
+
+    if not time:
+        print("Time must be provided.")
+        return
+        
+    time = float(time)
+
+    new_row = {'Product': new, 'Time(Mins)': time}
+    new_time.append(new_row, ignore_index=True)
+
+    return new_time, new_demands, new_profits
+
+    
 def create_output(lp, products, markets):
     """
     Creates output excel file for given lp problem and saves it to Tables folder. 
@@ -129,96 +229,121 @@ def create_output(lp, products, markets):
     output = output[cols]
 
     # write to excel file
-    output.to_excel("../Tables/output.xlsx", index=False)
+    output.to_excel(f"../Tables/{lp.name}_output.xlsx", index=False)
+
+    return output
+
+
+def solve_lp(name, time, demands, profits):
+    """
+    Solves and lp problem which has time, demands and profits dictionaries.
+    Saves output of lp problem as name_output.xlsx
+
+    Parameters:
+    str, pulp.LpProblem, 3 * dict[str, dict[str, list[float]]]
+
+    Returns:
+    None
+
+    """ 
+    products = list(time['Product'].values)
+
+    # read time table
+
+    time_dict = {}
+
+    for i in range(time.shape[0]):
+        time_dict[products[i]] = time.loc[i, 'Time(Mins)']
+
+    # read demands table
+    markets = list(demands['Areas'].values)
+
+    # rename sales areas to Area - i
+    markets = ["-".join(markets[i].split()[1:]) for i in range(len(markets))]
+
+    # read demand vaues
+
+    demands_dict = {}
+
+    for i in range(demands.shape[0]):
+        demand = {}
+        
+        for product in products:
+            demand[product] = demands.loc[i, product]
+
+        demands_dict[markets[i]] = demand
+
+    # read profit table
+    profits_dict = {}
+
+    for i in range(profits.shape[0]):
+        profit = {}
+        
+        for product in products:
+            profit[product] = profits.loc[i, product]
+
+        profits_dict[markets[i]] = profit
+
+    print("\nData used for lp: ")
+    print(time_dict)
+    print(demands_dict)
+    print(profits_dict)
+
+    # create lp problem
+    lp = pulp.LpProblem(name, sense=pulp.LpMaximize)
+
+    # add objective 
+    lp, variables = objective(lp, profits_dict)
+
+    # add constraints
+    lp = demand_constraits(lp, variables, demands_dict, products, markets)
+    lp = time_constraints(lp, variables, time_dict, demands_dict, products, markets)
+
+    # solve problem
+    lp.solve()
+
+    # print problem status
+    print(f"\nSolution Status for {name}: ", pulp.LpStatus[lp.status])
+
+    # print variable values
+    # for variable in lp.variables():
+    #     print("{} = {}".format(variable.name, variable.varValue))
+
+    # print objective value
+    print(f"\nObjective Value for {name}: ", pulp.value(lp.objective))
+
+    output = create_output(lp, products, markets)
+
+    print(output)
 
 
 # read excel files
 
-table_1 = pd.read_excel('../Tables/table_1.xlsx')
-table_2 = pd.read_excel('../Tables/table_2.xlsx')
-table_3 = pd.read_excel('../Tables/table_3.xlsx')
+if __name__ == '__main__':
 
-products = list(table_1['Product'].values)
+    time = pd.read_excel('../Tables/time.xlsx')
+    demands = pd.read_excel('../Tables/demands.xlsx')
+    profits = pd.read_excel('../Tables/profits.xlsx')
 
-# read time table - table_1
+    # do change here if you like add or change a product.
+    sensivitiy_analysis = False
 
-time = {}
+    if 'y' == input("Do you want to change any product (y/n): "):
+        sensivitiy_analysis = True
+        time_new, demands_new, profits_new = change_product(time, demands, profits)
 
-for i in range(table_1.shape[0]):
-    time[products[i]] = table_1.loc[i, 'Time(Mins)']
-
-# read demands table - table_2
-
-markets = list(table_2['Areas'].values)
-
-# rename sales areas to Area - i
-markets = ["-".join(markets[i].split()[1:]) for i in range(len(markets))]
-
-# read demand vaues
-
-demands = {}
-
-for i in range(table_2.shape[0]):
-    demand = {}
-    
-    for product in products:
-        demand[product] = table_2.loc[i, product]
-
-    demands[markets[i]] = demand
-
-# read profit table - table_3
-
-profits = {}
-
-for i in range(table_3.shape[0]):
-    profit = {}
-    
-    for product in products:
-        profit[product] = table_3.loc[i, product]
-
-    profits[markets[i]] = profit
+    if 'y' == input("Do you want to add any product (y/n): "):
+        sensivitiy_analysis = True
+        time_new, demands_new, profits_new = introduce_product(time_new, demands_new, profits_new)
 
 
-# products = ['A', 'B', 'C', 'D', 'E']
-# markets = ['1', '2', '3', '4']
+    if sensivitiy_analysis:
+        solve_lp("InitialProblem", time, demands, profits)
+        solve_lp("ChangedProblem", time_new, demands_new, profits_new)
+    else:
+        solve_lp("InitialProblem", time, demands, profits)
 
-# time = {'A': 1, 'B': 1.25, 'C': 0.75, 'D': 1.25, 'E': 1.2} 
 
-# demands = {'1': {'A': 6000, 'B': 3000, 'C': 6000, 'D': 5000, 'E': 0},
-#            '2': {'A': 2000, 'B': 0,    'C': 6000, 'D': 5000, 'E': 9000},
-#            '3': {'A': 4000, 'B': 1000, 'C': 0,    'D': 3000, 'E': 2000},
-#            '4': {'A': 0,    'B': 2000, 'C': 5000, 'D': 2000, 'E': 3000}}
-
-# profits = {'1': {'A': 4.80, 'B': 4.75, 'C': 3.75, 'D': 5.25, 'E': 0},
-#            '2': {'A': 3.75, 'B': 0,    'C': 3.30, 'D': 5.0,  'E': 5.04},
-#            '3': {'A': 4.0,  'B': 5.0,  'C': 0,    'D': 5.15, 'E': 5.25},
-#            '4': {'A': 0,    'B': 4.50, 'C': 3.50, 'D': 5.05, 'E': 5.15}}
-
-# create lp problem
-lp = pulp.LpProblem('Term-Project', sense=pulp.LpMaximize)
-
- 
-# add objective 
-lp, variables = objective(lp, profits)
-
-# add constraints
-lp = demand_constraits(lp, variables, demands, products, markets)
-lp = time_constraints(lp, variables, time, products, markets)
-
-# solve problem
-lp.solve()
-
-# print problem status
-print(pulp.LpStatus[lp.status])
-
-# print variable values
-for variable in lp.variables():
-    print("{} = {}".format(variable.name, variable.varValue))
-
-# print objective value
-print(pulp.value(lp.objective))
-
-create_output(lp, products, markets)
 
 
 
