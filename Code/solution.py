f19ed005d18b1d81eef37fcf3f30530dc72b1757
firat tamur <ftamur16@ku.edu.tr>
2020-05-31 11:15:09 +0300

    final

--- Code/solution.py
+++ Code/solution.py
@@ -1,9 +1,20 @@
+"""
+
+Indr262 - Spring 2020 
+Term Project
+
+Team Members:
+Melis Tuncay
+Asrın Şevval Aktaş
+Fırat Tamur
+
+"""
+
 # lp solver module
 import pulp
 
 # read excel file
 import pandas as pd
-
 
 # helper functions to build lp problem
 
@@ -243,7 +254,7 @@ def create_output(lp, demands):
     return output
 
 
-def check_demands(lp, demands):
+def check_demands(lp, demands, verbose=1):
     """
     Compares sum of each product in lp output and demands and finds 
     satisfied and unsatisfied products.
@@ -276,14 +287,14 @@ def check_demands(lp, demands):
         else:
             unsatisfied.append(product)
 
-
-    print("Satisfied Products: ", satisfied)
-    print("Unsatisfied Products: ", unsatisfied)
+    if verbose:
+        print("Satisfied Products: ", satisfied)
+        print("Unsatisfied Products: ", unsatisfied)
 
     return satisfied, unsatisfied
 
 
-def solve_lp(name, time, demands, profits):
+def solve_lp(name, time, demands, profits, verbose=1):
     """
     Solves and lp problem which has time, demands and profits dictionaries.
     Saves output of lp problem as name_output.xlsx
@@ -346,28 +357,76 @@ def solve_lp(name, time, demands, profits):
     # solve problem
     lp.solve()
 
-    # print problem status
-    print(f"\nSolution Status for {name}: ", pulp.LpStatus[lp.status])
-
-    # print objective value
-    print(f"\nObjective Value for {name}: ", pulp.value(lp.objective))
-
+    # find output demand values
     output = create_output(lp, demands)
-    check_demands(lp, demands)
 
-    print(output)
+    # find satisfied and unsatisfied products.
+    satisfied, unsatisfied = check_demands(lp, demands, verbose=0)
+
+    if verbose:
+        # satisfied products
+        print("\nSatisfied Products: ", satisfied)
+        
+        # print problem status
+        print(f"\nSolution Status for {name}: {pulp.LpStatus[lp.status]}")
+
+        # print objective value
+        print(f"\nObjective Value for {name}: {pulp.value(lp.objective)}\n")
+
+        # output demand values
+        print(output)
+        print() 
 
     # return lp problem.
-    return lp
+    return lp, satisfied, unsatisfied
+
+
+def write_lp_txt(name, lp, time, demands, profits, write='w'):
+    """
+    Writes result of a lp problem to txt file.
+
+    """
+    with open("part-e.txt", write) as file:
+        
+        file.write(f"Tables Used for problem:  {name}\n")
+        file.write(name)
+        file.write(time.to_csv(sep=' ', index=False, header=True))
+        file.write("\n")
+        file.write(demands.to_csv(sep=' ', index=False, header=True))
+        file.write("\n")
+        file.write(profits.to_csv(sep=' ', index=False, header=True))
+        file.write("\n")  
+
+        output = create_output(lp, demands)
+        satisfied, unsatisfied = check_demands(lp, demands, verbose=0)
+
+        file.write("Results:\n")
+
+        file.write(f"Status: {pulp.LpStatus[lp.status]}\n")
+        file.write(f"Objective: {pulp.value(lp.objective)}\n")
+        file.write(f"Satisfied Products: {satisfied}\n\n")
+
+        file.write("Output Table: \n")
+    
+        file.write(output.to_csv(sep=' ', index=False, header=True))
+        file.write("\n")   
+
+
 
 if __name__ == '__main__':
     
     # read excel files
-    time = pd.read_excel('../Tables/time.xlsx')
-    demands = pd.read_excel('../Tables/demands.xlsx')
-    profits = pd.read_excel('../Tables/profits.xlsx')
 
-    # do change here if you like add or change a product.
+    try:
+        time = pd.read_excel('../Tables/time.xlsx')
+        demands = pd.read_excel('../Tables/demands.xlsx')
+        profits = pd.read_excel('../Tables/profits.xlsx')
+    except:
+        print("Tables not found. Wrong File Paths.")
+    finally:
+        exit()
+
+    # if a product change of a new product comes set it True.
     sensivitiy_analysis = False
 
     time_new, demands_new, profits_new = time, demands, profits
@@ -385,19 +444,19 @@ if __name__ == '__main__':
     print(demands)
     print(profits)
 
+    # if any product has changed or new product added.
     if sensivitiy_analysis:
-        init_lp = solve_lp("InitialProblem", time, demands, profits)
+        init_lp, sat, unsat = solve_lp("InitialProblem", time, demands, profits)
+        write_lp_txt("InitialProblem", init_lp, time, demands, profits, 'w')
     
-        print("\n\nTables used for Modified Problem:")
+        print("\n\nTables used for Changed Problem:")
         print(time_new)
         print(demands_new)
         print(profits_new)
     
-        changed_lp = solve_lp("ChangedProblem", time_new, demands_new, profits_new)
+        changed_lp, sat, unsat = solve_lp("ChangedProblem", time_new, demands_new, profits_new)
+        write_lp_txt("ChangedProblem", changed_lp, time_new, demands_new, profits_new, 'a')
+
     else:
-        init_lp = solve_lp("InitialProblem", time, demands, profits)
-
-
-
-
-
+        init_lp, sat, unsat = solve_lp("InitialProblem", time, demands, profits)
+        write_lp_txt("InitialProblem", init_lp, time, demands, profits, 'w')
